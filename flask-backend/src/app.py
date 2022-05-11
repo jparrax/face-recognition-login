aef95fd110e270764e5f3474da287df27f8888bf
Juan Fernando Parra <jparrax17@gmail.com>
2022-05-11 14:39:16 +1200

    Complete sample code
    - Compare a static face with and image sent by the frontend.

--- flask-backend/src/app.py
+++ flask-backend/src/app.py
@@ -1,32 +1,26 @@
 from flask import Flask, Response, jsonify, request
 import numpy as np
+import face_recognition
 import cv2
-
-# Get user supplied values
-cascPath = "haarcascade_frontalface_default.xml"
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
-
 
 app = Flask(__name__)
 
 @app.route("/authenticate", methods=['POST'])
 def authenticate():
 
-    face_image_bytes = request.files['face'].stream.read()
-    nparr = np.fromstring(face_image_bytes, np.uint8)
+    base_img = face_recognition.load_image_file("C:\\Users\\parraua\\Documents\\GitHub\\face-recognition-login\\flask-backend\\src\\assets\\images\\juan\\0.jpg")
+    base_img = cv2.cvtColor(base_img, cv2.COLOR_BGR2RGB)
 
-    # # Read the image
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    encode_my_face = face_recognition.face_encodings(base_img)[0]
 
-    # Detect faces in the image
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
+    sample_image_bytes = request.files['face'].stream.read()
+    sample_image_np_array = np.fromstring(sample_image_bytes, np.uint8)
 
-    return jsonify(data = f"Found {len(faces)} faces!")
+    sample_image = cv2.imdecode(sample_image_np_array, cv2.IMREAD_COLOR)
+    sample_image = cv2.cvtColor(sample_image, cv2.COLOR_BGR2RGB)
+
+    encode_sample_face = face_recognition.face_encodings(sample_image)[0]
+
+    result = face_recognition.compare_faces([encode_my_face], encode_sample_face)
+
+    return jsonify(data = str(result[0]))
